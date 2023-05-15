56e4f211f3672e37e268c8409e8602bd0a4693a7
Barış Atasoy <109789408+the-atasoy@users.noreply.github.com>
2023-05-15 13:54:19 +0300

    AddCarPanel has been started to design

--- Cars/AddCarPanel.Designer.cs
+++ Cars/AddCarPanel.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddCarPanel));
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.cars_label = new System.Windows.Forms.Label();
+            this.makeTextbox = new System.Windows.Forms.TextBox();
+            this.modelTextbox = new System.Windows.Forms.TextBox();
+            this.yearTextbox = new System.Windows.Forms.TextBox();
+            this.hpTextbox = new System.Windows.Forms.TextBox();
+            this.versionTextbox = new System.Windows.Forms.TextBox();
+            this.colorTextbox = new System.Windows.Forms.TextBox();
+            this.kmTextbox = new System.Windows.Forms.TextBox();
+            this.priceTextbox = new System.Windows.Forms.TextBox();
+            this.ccTextbox = new System.Windows.Forms.TextBox();
+            this.addCarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logo
+            // 
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(13, 13);
+            this.logo.Margin = new System.Windows.Forms.Padding(4);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(75, 44);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 9;
+            this.logo.TabStop = false;
+            this.logo.Click += new System.EventHandler(this.logo_Click);
+            // 
+            // cars_label
+            // 
+            this.cars_label.AutoSize = true;
+            this.cars_label.Font = new System.Drawing.Font("Magneto", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cars_label.Location = new System.Drawing.Point(96, 24);
+            this.cars_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.cars_label.Name = "cars_label";
+            this.cars_label.Size = new System.Drawing.Size(99, 22);
+            this.cars_label.TabIndex = 8;
+            this.cars_label.Text = "car$.com";
+            // 
+            // makeTextbox
+            // 
+            this.makeTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.makeTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.makeTextbox.Location = new System.Drawing.Point(12, 106);
+            this.makeTextbox.Name = "makeTextbox";
+            this.makeTextbox.Size = new System.Drawing.Size(167, 28);
+            this.makeTextbox.TabIndex = 10;
+            this.makeTextbox.TextChanged += new System.EventHandler(this.makeTextbox_TextChanged);
+            // 
+            // modelTextbox
+            // 
+            this.modelTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.modelTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modelTextbox.Location = new System.Drawing.Point(12, 161);
+            this.modelTextbox.Name = "modelTextbox";
+            this.modelTextbox.Size = new System.Drawing.Size(167, 28);
+            this.modelTextbox.TabIndex = 10;
+            this.modelTextbox.TextChanged += new System.EventHandler(this.modelTextbox_TextChanged);
+            // 
+            // yearTextbox
+            // 
+            this.yearTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.yearTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearTextbox.Location = new System.Drawing.Point(12, 214);
+            this.yearTextbox.Name = "yearTextbox";
+            this.yearTextbox.Size = new System.Drawing.Size(167, 28);
+            this.yearTextbox.TabIndex = 10;
+            this.yearTextbox.TextChanged += new System.EventHandler(this.yearTextbox_TextChanged);
+            // 
+            // hpTextbox
+            // 
+            this.hpTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.hpTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hpTextbox.Location = new System.Drawing.Point(413, 161);
+            this.hpTextbox.Name = "hpTextbox";
+            this.hpTextbox.Size = new System.Drawing.Size(167, 28);
+            this.hpTextbox.TabIndex = 10;
+            this.hpTextbox.TextChanged += new System.EventHandler(this.hpTextbox_TextChanged);
+            // 
+            // versionTextbox
+            // 
+            this.versionTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.versionTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionTextbox.Location = new System.Drawing.Point(213, 106);
+            this.versionTextbox.Name = "versionTextbox";
+            this.versionTextbox.Size = new System.Drawing.Size(167, 28);
+            this.versionTextbox.TabIndex = 10;
+            this.versionTextbox.TextChanged += new System.EventHandler(this.versionTextbox_TextChanged);
+            // 
+            // colorTextbox
+            // 
+            this.colorTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.colorTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorTextbox.Location = new System.Drawing.Point(213, 161);
+            this.colorTextbox.Name = "colorTextbox";
+            this.colorTextbox.Size = new System.Drawing.Size(167, 28);
+            this.colorTextbox.TabIndex = 10;
+            this.colorTextbox.TextChanged += new System.EventHandler(this.colorTextbox_TextChanged);
+            // 
+            // kmTextbox
+            // 
+            this.kmTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.kmTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kmTextbox.Location = new System.Drawing.Point(213, 214);
+            this.kmTextbox.Name = "kmTextbox";
+            this.kmTextbox.Size = new System.Drawing.Size(167, 28);
+            this.kmTextbox.TabIndex = 10;
+            this.kmTextbox.TextChanged += new System.EventHandler(this.kmTextbox_TextChanged);
+            // 
+            // priceTextbox
+            // 
+            this.priceTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.priceTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priceTextbox.Location = new System.Drawing.Point(413, 214);
+            this.priceTextbox.Name = "priceTextbox";
+            this.priceTextbox.Size = new System.Drawing.Size(167, 28);
+            this.priceTextbox.TabIndex = 10;
+            this.priceTextbox.TextChanged += new System.EventHandler(this.priceTextbox_TextChanged);
+            // 
+            // ccTextbox
+            // 
+            this.ccTextbox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ccTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ccTextbox.Location = new System.Drawing.Point(413, 106);
+            this.ccTextbox.Name = "ccTextbox";
+            this.ccTextbox.Size = new System.Drawing.Size(167, 28);
+            this.ccTextbox.TabIndex = 10;
+            this.ccTextbox.TextChanged += new System.EventHandler(this.ccTextbox_TextChanged);
+            // 
+            // addCarButton
+            // 
+            this.addCarButton.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.addCarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCarButton.Location = new System.Drawing.Point(261, 260);
+            this.addCarButton.Name = "addCarButton";
+            this.addCarButton.Size = new System.Drawing.Size(72, 66);
+            this.addCarButton.TabIndex = 11;
+            this.addCarButton.Text = "Add Car";
+            this.addCarButton.UseVisualStyleBackColor = false;
+            this.addCarButton.Click += new System.EventHandler(this.addCarButton_Click);
+            // 
+            // AddCarPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(593, 338);
+            this.Controls.Add(this.addCarButton);
+            this.Controls.Add(this.priceTextbox);
+            this.Controls.Add(this.hpTextbox);
+            this.Controls.Add(this.kmTextbox);
+            this.Controls.Add(this.yearTextbox);
+            this.Controls.Add(this.colorTextbox);
+            this.Controls.Add(this.ccTextbox);
+            this.Controls.Add(this.versionTextbox);
+            this.Controls.Add(this.modelTextbox);
+            this.Controls.Add(this.makeTextbox);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.cars_label);
+            this.MaximizeBox = false;
+            this.Name = "AddCarPanel";
             this.Text = "AddCarPanel";
+            this.Load += new System.EventHandler(this.AddCarPanel_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Label cars_label;
+        private System.Windows.Forms.TextBox makeTextbox;
+        private System.Windows.Forms.TextBox modelTextbox;
+        private System.Windows.Forms.TextBox yearTextbox;
+        private System.Windows.Forms.TextBox hpTextbox;
+        private System.Windows.Forms.TextBox versionTextbox;
+        private System.Windows.Forms.TextBox colorTextbox;
+        private System.Windows.Forms.TextBox kmTextbox;
+        private System.Windows.Forms.TextBox priceTextbox;
+        private System.Windows.Forms.TextBox ccTextbox;
+        private System.Windows.Forms.Button addCarButton;
     }
 }
--- Cars/AddCarPanel.cs
+++ Cars/AddCarPanel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,145 @@ namespace Cars
         public AddCarPanel()
         {
             InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-7B11AB0;Initial Catalog=cars.com;Integrated Security=True");
+
+        private void logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void priceTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kmTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yearTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hpTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ccTextbox_TextChanged(object sender, EventArgs e)
+        {
+            ccTextbox.Text = string.Empty;
+        }
+
+        private void versionTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modelTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void makeTextbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addCarButton_Click(object sender, EventArgs e)
+        {
+            string make = makeTextbox.Text;
+            string model = modelTextbox.Text;
+            string year = yearTextbox.Text;
+            string version = versionTextbox.Text;
+            string color = colorTextbox.Text;
+            string km = kmTextbox.Text;
+            string cc = ccTextbox.Text;
+            string hp = hpTextbox.Text;
+            string price = priceTextbox.Text;
+
+            string query = "INSERT INTO cars (make, model, year, version, color, km, cylinder_capacity, hp, price) " +
+                           "VALUES (@make, @model, @year, @version, @color, @km, @cc, @hp, @price)";
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@make", make);
+            cmd.Parameters.AddWithValue("@model", model);
+            cmd.Parameters.AddWithValue("@year", year);
+            cmd.Parameters.AddWithValue("@version", version);
+            cmd.Parameters.AddWithValue("@color", color);
+            cmd.Parameters.AddWithValue("@km", km);
+            cmd.Parameters.AddWithValue("@cc", cc);
+            cmd.Parameters.AddWithValue("@hp", hp);
+            cmd.Parameters.AddWithValue("@price", price);
+
+            try
+            {
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Car added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ClearCarForm();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to add car.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding car: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void ClearCarForm()
+        {
+            makeTextbox.Clear();
+            modelTextbox.Clear();
+            yearTextbox.Clear();
+            versionTextbox.Clear();
+            colorTextbox.Clear();
+            kmTextbox.Clear();
+            ccTextbox.Clear();
+            hpTextbox.Clear();
+            priceTextbox.Clear();
+        }
+
+        private void ccTextbox_Enter(object sender, EventArgs e)
+        {
+            if (ccTextbox.Text == "Enter Cylinder Capacity")
+            {
+                ccTextbox.Text = "";
+                ccTextbox.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void ccTextbox_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(ccTextbox.Text))
+            {
+                ccTextbox.Text = "Enter Cylinder Capacity";
+                ccTextbox.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void AddCarPanel_Load(object sender, EventArgs e)
+        {
+            ccTextbox_Enter(sender, e);
+            ccTextbox_Leave(sender, e);
         }
     }
 }
